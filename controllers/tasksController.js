44de695c326bde7aac7d5325d096875751bdb28f
Ismail <kherkheulidze.i@amayasoft.uz>
2023-12-09 22:35:49 +0500

    Controller development is complete

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -9,6 +9,39 @@ class TasksController {
       throw e
     }
   }
+
+  async createTask(req, res) {
+    try {
+      const { title, body } = req.body
+      const task = await models.Task.create({ title, body })
+      return res.json(task)
+    } catch (e) {
+      return res.json(e)
+    }
+  }
+
+  async updateTask(req, res) {
+    try {
+      const { id } = req.body
+
+      await models.Task.update(req.body, { where: { id: id } })
+
+      return res.status(200).json({ message: 'Task was updated' })
+    } catch (e) {
+      return res.json(e)
+    }
+  }
+
+  async deleteTask(req, res) {
+    try {
+      const { id } = req.body
+      await models.Task.destroy({ where: { id: id } })
+
+      return res.status(200).json({ message: 'Task was deleted' })
+    } catch (e) {
+      throw e
+    }
+  }
 }
 
 export default new TasksController()
--- routes/tasksRouter.js
+++ routes/tasksRouter.js
@@ -4,5 +4,8 @@ import tasksController from '../controllers/tasksController.js'
 const router = Router()
 
 router.get('/', tasksController.getTasks)
+router.post('/create-task', tasksController.createTask)
+router.put('/update-task', tasksController.updateTask)
+router.delete('/delete-task', tasksController.deleteTask)
 
 export default router
